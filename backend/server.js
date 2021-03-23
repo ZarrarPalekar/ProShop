9ce349abdfe3c7e3408a0c3d0db95b6508c89a65
Zarrar Palekar <zarrar@live.in>
2021-03-23 19:10:00 +0530

    Small Change

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,13 @@ import express, { json } from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import morgan from "morgan";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
-
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
