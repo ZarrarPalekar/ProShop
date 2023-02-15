48ce4bf009798bab1e157e849b97871ee9f8c3e2
Zarrar Palekar <zarrar.palekar@azularc.com>
2023-02-16 00:18:36 +0530

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,23 @@
 // import mongoose from "mongoose";
 import pkg from "mongodb";
-const {MongoClient} = pkg;
+const { MongoClient } = pkg;
 
 const connectDB = async () => {
   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useUnifiedTopology: true,
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//     });
-    
-    const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri);
-    const PORT = process.env.PORT || 5000;
-    
-    client.connect(err => {
-    if(err){ console.error(err); return false;}
-    // connection to mongo is successful, listen for requests
-//     app.listen(PORT, () => {
-//         console.log("listening for requests");
-//     })
-});
+    //     const conn = await mongoose.connect(process.env.MONGO_URI, {
+    //       useUnifiedTopology: true,
+    //       useNewUrlParser: true,
+    //       useCreateIndex: true,
+    //     });
 
+    const uri = process.env.MONGO_URI;
+    const client = new MongoClient(uri);
+    const PORT = process.env.PORT || 5000;
+
+    client
+      .connect()
+      .then(() => console.log("connected"))
+      .catch((e) => console.log(e));
 
     console.log(`MongoDB Connected: `.cyan.underline);
   } catch (error) {
